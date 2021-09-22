8af7395223dca82b45e149662d9b36f070564059
cmitchell14 <calmitch14@gmail.com>
2021-09-22 16:12:42 -0500

    Deleted default files, ready for action

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css'; -- Deleted file
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals'; --Deleted File
+
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(
   <React.StrictMode>
